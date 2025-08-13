ae2b3181a8fbfe01c5aa789da170ffae7bb2e5bd
zzz <zhaoyuxuan233@qq.com>
2025-08-13 15:16:09 +0800

    zzzの提交测试
    我创建了一个UI面板，仅此而已

---
+++ CMGM_Frame_0_2_0/Assets/_WorkSpace/Scripts/GameUI/Panels/zzzTextPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class zzzTextPanel : BasePanel
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void OnButtonClick(string btnName)
+    {
+        switch(btnName)
+        {
+            case "biggerBtn":
+                UIManager.Instance.HidePanel<zzzTextPanel>();
+                break;
+            default:
+                break;
+        }
+    }
+}
