050cec6cdc6e17317e587996061349e489fb7570
MYDQC <zenvan.00@gmail.com>
2026-06-22 15:58:43 +0800

    Revert "加wwise_authoring"
    This reverts commit b6859eb53d5d30a792d6c44d6a429eb2459c2614.

--- CMGM_Frame/Assets/Wwise/API/Editor/AkSoundBankDirectoryWatcher.cs
+++ CMGM_Frame/Assets/Wwise/API/Editor/AkSoundBankDirectoryWatcher.cs
@@ -19,7 +19,7 @@ using System;
 using System.Threading.Tasks;
 using UnityEngine;
 
-#if UNITY_EDITOR && WWISE_AUTHORING
+#if UNITY_EDITOR
 namespace Wwise.API.Editor.SoundBankDirectoryWatcher.Common
 {
 	[UnityEditor.InitializeOnLoad]
